651cccbaee346b5eb1a3d97c9714e2c9eeecbad9
Hungtai.chen <hungtai.chen@gmail.com>
2018-12-25 21:05:57 +0800

    Update 管理者login.php

--- 管理者login.php
+++ 管理者login.php
@@ -9,7 +9,7 @@
 
         <link rel="stylesheet" type="text/css" href="\css\bootstrap.min.css">
 
-        <link rel="stylesheet" type="text/css" href="\css\line-icons.css">
+        <link rel="stylesheet" type="text/css" href="\fonts\line-icons.css">
 
         <link rel="stylesheet" type="text/css" href="\css\slicknav.css">
 
@@ -311,7 +311,9 @@
             <div class="loader" id="loader-1"></div>
         </div>
 
-
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
         <script data-cfasync="false" src="\js\email-decode.min.js"></script>
         <script src="\js\jquery-min.js"></script>
         <script src="\js\popper.min.js"></script>

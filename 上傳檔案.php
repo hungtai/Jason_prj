595cba82695e27a50f6b47c3d7d9f04f6ff6148f
Hungtai.chen <hungtai.chen@gmail.com>
2018-12-25 21:09:23 +0800

    Update 上傳檔案.php

--- 上傳檔案.php
+++ 上傳檔案.php
@@ -19,7 +19,7 @@
         <!--[if lt IE 9]>
           <script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	</head>
 
     <body id="page-top">

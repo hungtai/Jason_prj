0434757c74add9d23857e0eb8508fd2a0f5c6512
Hungtai.chen <hungtai.chen@gmail.com>
2018-12-26 11:05:12 +0800

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -38,7 +38,7 @@
 		
 		print "<pre>\n";
 		
-		echo "$_POST['id'].$_POST['name'].$_POST['password'].$_POST['roleSelect']"
+		echo "$_POST['id']"
 	    	if (!pg_num_rows($result)) {
 			print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 		} 

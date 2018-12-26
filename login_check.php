c15915f61ceb1f7f7c9ee67af8bd522e5a60761f
Hungtai.chen <hungtai.chen@gmail.com>
2018-12-26 15:27:36 +0800

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -12,19 +12,18 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 				
 if ($_POST['roleSelect'] == "學生") {
 	$id =(string)$_POST['UserID'];
-	$name = (string)$_POST['name'];
 	$password = (string)$_POST['Password']; 
 	//print_r($_POST);
 			
 	//$sql = "INSERT INTO student (id,name,password) VALUES ($id,$name,$password)";
-	$result = pg_query($pg_conn, "INSERT INTO student (id,name,password) VALUES ('$id','$name','$password')");
+	$result = pg_query($pg_conn, "SELECT * FROM student WHERE id ='$id' AND password = '$password'");
 			
 	if (!$result){
 		$res1 = pg_get_result($pg_conn);
 		echo pg_result_error($res1);
 		exit;
 	}
-	echo "<script>alert('學生帳號建立成功!');location.href='".$_SERVER["HTTP_REFERER"]."';</script>";
+	echo "<script>alert('學生帳號存在, 導向至學生功能頁面!');location.href='".$_SERVER["HTTP_REFERER"]."';</script>";
 }
 if ($_POST['roleSelect'] == "教師") {
 	$id =(string)$_POST['UserID'];
